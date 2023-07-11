86c303dc704123d5470c236f41d8cb6984f90326
Darshan Patil <iampatildarsh>
2023-07-11 15:06:13 +0530

    changing index of mysql auto increment

--- src/main/java/com/example/crudDemo/CrudDemoApplication.java
+++ src/main/java/com/example/crudDemo/CrudDemoApplication.java
@@ -16,8 +16,25 @@ public class CrudDemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
 		return runner -> {
-			createStudent(studentDAO);
+			//createStudent(studentDAO);
+			createMultipleStudent(studentDAO);
 		};
+	}
+
+	private void createMultipleStudent(StudentDAO studentDAO) {
+
+		//create multiple students
+
+		System.out.println("Creating 3 student object ...");
+		Student tempStudent1 = new Student("dimpal", "patil", "def@gmail.com");
+		Student tempStudent2 = new Student("sanjay", "patil", "ghi@gmail.com");
+		Student tempStudent3 = new Student("nilima", "patil", "lmn@gmail.com");
+
+		//saving the student object
+		System.out.println("Saving the students...");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
 	}
 
 	private void createStudent(StudentDAO studentDAO) {
